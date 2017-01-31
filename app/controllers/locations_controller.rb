ffa0e48757b3f98d692bee0fc7ae50610d35f642
doctor3397 <hs2546@columbia.edu>
2017-01-31 15:35:16 -0500

    Add routes and location_controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,17 +1,21 @@
 class LocationsController < ApplicationController
+  #  GET   /locations  locations#index
   def index
     @locations = Location.all
   end
 
+  # GET   /locations/:id  locations#show
   def show
     @location = Location.find(params[:id])
     @events = @location.events
   end
 
+  # GET    /locations/:id/edit    locations#edit
   def edit
     @location = Location.find(params[:id])
   end
 
+  #   PATCH  /locations/:id    locations#update
   def update
     @location = Location.find(params[:id])
     if @location.update(location_params)
@@ -21,10 +25,12 @@ class LocationsController < ApplicationController
     end
   end
 
+  # GET    /locations/new   locations#new
   def new
     @location = Location.new
   end
 
+ #   POST   /locations   locations#create
   def create
     @location = Location.new(location_params)
 
@@ -35,6 +41,7 @@ class LocationsController < ApplicationController
     end
   end
 
+  # DELETE /locations/:id   locations#destroy
   def destroy
     @location = Location.find(params[:id])
     @location.destroy
--- config/routes.rb
+++ config/routes.rb
@@ -12,3 +12,34 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+=begin
+Prefix Verb   URI Pattern                                       Controller#Action
+               root GET    /                                                 home#index
+         home_index GET    /home/index(.:format)                             home#index
+    location_events GET    /locations/:location_id/events(.:format)          events#index
+                    POST   /locations/:location_id/events(.:format)          events#create
+ new_location_event GET    /locations/:location_id/events/new(.:format)      events#new
+edit_location_event GET    /locations/:location_id/events/:id/edit(.:format) events#edit
+     location_event GET    /locations/:location_id/events/:id(.:format)      events#show
+                    PATCH  /locations/:location_id/events/:id(.:format)      events#update
+                    PUT    /locations/:location_id/events/:id(.:format)      events#update
+                    DELETE /locations/:location_id/events/:id(.:format)      events#destroy
+          locations GET    /locations(.:format)                              locations#index
+                    POST   /locations(.:format)                              locations#create
+       new_location GET    /locations/new(.:format)                          locations#new
+      edit_location GET    /locations/:id/edit(.:format)                     locations#edit
+           location GET    /locations/:id(.:format)                          locations#show
+                    PATCH  /locations/:id(.:format)                          locations#update
+                    PUT    /locations/:id(.:format)                          locations#update
+                    DELETE /locations/:id(.:format)                          locations#destroy
+       example_form GET    /example/form(.:format)                           example#form
+   job_applications GET    /job_applications(.:format)                       job_applications#index
+                    POST   /job_applications(.:format)                       job_applications#create
+new_job_application GET    /job_applications/new(.:format)                   job_applications#new
+edit_job_application GET    /job_applications/:id/edit(.:format)              job_applications#edit
+    job_application GET    /job_applications/:id(.:format)                   job_applications#show
+                    PATCH  /job_applications/:id(.:format)                   job_applications#update
+                    PUT    /job_applications/:id(.:format)                   job_applications#update
+                    DELETE /job_applications/:id(.:format)                   job_applications#destroy
+=end
